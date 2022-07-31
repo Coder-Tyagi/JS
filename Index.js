1d6ee79ce6f0d96e8458147a6f37032b07fc93e2
Akshay Tyagi <akshaybadholi@gmail.com>
2022-08-01 01:53:47 +0530

    SetTimeout added

--- Index.js
+++ Index.js
@@ -1,9 +1,13 @@
-var outer = ()=>{
-    var a=5;
-    function inner(){
+var outer = () => {
+    var a = 5;
+    function inner() {
         console.log(a);
     }
     inner();
 
 }
 outer();
+
+setTimeout(function () {
+    console.log("state");
+}, 1000)
